961e985723c2786de0bc727af603c4ff3b640c79
mrjzhang <mrjzhang@tencent.com>
2020-01-05 20:49:11 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: 'yiliang',
+  description: '嘻嘻',
+  base: '/blog/',
+  head: [
+    [
+      'link',
+      { rel: 'shortcut icon', href: '/favicon.icon.ico', type: 'image/jpg' }
+    ]
+  ],
+  themeConfig: {
+    logo: '/assets/img/logo.jpg',
+    smoothScroll: true,
+    nav: [
+      { text: '知识库', link: '/' },
+      { text: '工具', link: '/guide/' },
+      {
+        text: '我',
+        link: 'Github'
+      }
+    ],
+    sidebar: {}
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+actionText: 去康康 →
+actionLink: /guide
+features:
+  footer: MIT Licensed | Copyright © 2019-present
+---
