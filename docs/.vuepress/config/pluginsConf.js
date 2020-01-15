9af32bb367385933f814c99eb1bca181dfdd44a3
yiliang114 <1204183885@qq.com>
2020-01-15 19:53:50 +0800

    feat: add vuepress plugins

--- docs/.vuepress/config/pluginsConf.js
+++ docs/.vuepress/config/pluginsConf.js
@@ -7,5 +7,20 @@ module.exports = [
     {
       ga: "UA-156101458-1"
     }
-  ]
+  ],
+  // vuepress-plugin-dynamic-title
+  [
+    "dynamic-title",
+    {
+      showIcon: "/favicon.ico",
+      showText: "(/≧▽≦/)咦！又好了！",
+      hideIcon: "/failure.ico",
+      hideText: "(●—●)喔哟，崩溃啦！",
+      recoverTime: 2000
+    }
+  ],
+  // vuepress-plugin-cursor-effects
+  "cursor-effects",
+  // vuepress-plugin-go-top
+  "go-top"
 ];
