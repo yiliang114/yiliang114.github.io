3f6e82c22c8bd1c62348e8676502c9edf4a8c188
yiliang114 <1204183885@qq.com>
2020-01-29 15:04:21 +0800

    chore: add readme script

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: mrjzhang
  * @Date: 2020-01-28 21:06:46
  * @LastEditors  : mrjzhang
- * @LastEditTime : 2020-01-28 21:16:49
+ * @LastEditTime : 2020-01-29 15:02:21
  */
 const headConfig = require("./config/headConf.js");
 const pluginsConf = require("./config/pluginsConf.js");
@@ -24,19 +24,43 @@ module.exports = {
           title: "前端",
           collapsable: false,
           children: [
-            "campus-recruitment",
-            "vue-thanos-snap",
+            "Vue-Script-Static-file-Uncaught-SyntaxError",
             "browser-cache",
+            "campus-recruitment",
+            "can-not-find-js-or-css-files-of-nginx",
             "code-split",
-            "vue-cli@2.x-upgrade",
+            "common-use-to-cross-domain-for-fe",
+            "css-show-ellipsis-when-multi-line",
             "decorator-error",
-            "mobx@4.x-Array.map-in-strict"
+            "element-table-content-newline",
+            "mobx@4.x-Array.map-in-strict",
+            "vue-cli@2.x-upgrade",
+            "vue-import-ttf-font",
+            "vue-input-enter-event-in-chinese-method",
+            "vue-style-background-image-blank",
+            "vue-thanos-snap"
           ]
         }
       ],
       "/ios-and-android/": ["127.0.0.1-404"],
-      "/code-and-life/": ["cname-forget"],
-      "/tools/": ["light-cvm", "mac-install-sublime", "fanqiang"]
+      "/code-and-life/": ["cname-forget", "technical-debt"],
+      "/tools/": [
+        "add-subline-into-right-hand-menu",
+        "add-vscode-into-right-hand-menu",
+        "fanqiang",
+        "jetbrains-exclude-node_modules",
+        "light-cvm",
+        "mac-install-sublime",
+        "solo-migration",
+        "vim",
+        "vscode-vue-file-template"
+      ],
+      "/back-end/": ["linux-iptables", "ubuntu-docker-jenkins"],
+      "/go/": [
+        "environment-setup-for-mac-develper",
+        "environment-setup-for-win-develper"
+      ],
+      "/node/": ["process-env"]
     },
     nav: [
       { text: "front-end", link: "/front-end/browser-cache" },
---
+++ readFiles.js
@@ -0,0 +1,36 @@
+/*
+ * @Author: mrjzhang
+ * @Date: 2019-11-16 20:21:25
+ * @LastEditors  : mrjzhang
+ * @LastEditTime : 2020-01-29 14:56:17
+ */
+const fs = require("fs");
+const join = require("path").join;
+
+const excludeDirs = [".vuepress", "node_modules", ".git"];
+
+function getJsonFiles(jsonPath) {
+  let jsonFiles = [];
+  function findJsonFile(path) {
+    const files = fs.readdirSync(path),
+      len = files.length;
+    for (let i = 0; i < len; i++) {
+      let item = files[i];
+      let fPath = join(path, item);
+      let stat = fs.statSync(fPath);
+      if (stat.isDirectory() && !excludeDirs.includes(fPath)) {
+        findJsonFile(fPath);
+      }
+      if (stat.isFile() && fPath.endsWith(".md")) {
+        // 如果发现是一个 md 文件的话， 就处理其中的图片
+        jsonFiles.push(fPath);
+      }
+    }
+  }
+
+  findJsonFile(jsonPath);
+  const result = jsonFiles.join("\r\n");
+  fs.writeFileSync("./files.txt", result);
+}
+
+getJsonFiles("docs");
