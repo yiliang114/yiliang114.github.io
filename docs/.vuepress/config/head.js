d8fc46296332bec1eab8e9ca7fd0cea26a5a3b51
mrjzhang <mrjzhang@tencent.com>
2020-07-09 22:25:00 +0800

    chore(eslint): code format

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -1,22 +1,19 @@
 module.exports = [
+  ['link', { rel: 'shortcut icon', href: '/favicon.icon.ico', type: 'image/jpg' }],
   [
-    "link",
-    { rel: "shortcut icon", href: "/favicon.icon.ico", type: "image/jpg" }
-  ],
-  [
-    "meta",
+    'meta',
     {
-      name: "keywords",
+      name: 'keywords',
       content:
-        "前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown"
+        '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown'
     }
   ],
   // 不蒜子统计
   [
-    "script",
+    'script',
     {
       async: true,
-      src: "//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"
+      src: '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'
     }
   ]
-];
+]
--- docs/.vuepress/config/sidebar-copy.js
+++ docs/.vuepress/config/sidebar-copy.js
@@ -1,44 +1,41 @@
 module.exports = {
-  "/front-end/": [
+  '/front-end/': [
     {
-      title: "前端",
+      title: '前端',
       collapsable: false,
       children: [
-        "Vue-Script-Static-file-Uncaught-SyntaxError",
-        "browser-cache",
-        "campus-recruitment",
-        "can-not-find-js-or-css-files-of-nginx",
-        "code-split",
-        "common-use-to-cross-domain-for-fe",
-        "css-show-ellipsis-when-multi-line",
-        "decorator-error",
-        "element-table-content-newline",
-        "mobx@4.x-Array.map-in-strict",
-        "vue-cli@2.x-upgrade",
-        "vue-import-ttf-font",
-        "vue-input-enter-event-in-chinese-method",
-        "vue-style-background-image-blank",
-        "vue-thanos-snap"
+        'Vue-Script-Static-file-Uncaught-SyntaxError',
+        'browser-cache',
+        'campus-recruitment',
+        'can-not-find-js-or-css-files-of-nginx',
+        'code-split',
+        'common-use-to-cross-domain-for-fe',
+        'css-show-ellipsis-when-multi-line',
+        'decorator-error',
+        'element-table-content-newline',
+        'mobx@4.x-Array.map-in-strict',
+        'vue-cli@2.x-upgrade',
+        'vue-import-ttf-font',
+        'vue-input-enter-event-in-chinese-method',
+        'vue-style-background-image-blank',
+        'vue-thanos-snap'
       ]
     }
   ],
-  "/ios-and-android/": ["127.0.0.1-404"],
-  "/code-and-life/": ["cname-forget", "technical-debt"],
-  "/tools/": [
-    "add-subline-into-right-hand-menu",
-    "add-vscode-into-right-hand-menu",
-    "fanqiang",
-    "jetbrains-exclude-node_modules",
-    "light-cvm",
-    "mac-install-sublime",
-    "solo-migration",
-    "vim",
-    "vscode-vue-file-template"
+  '/ios-and-android/': ['127.0.0.1-404'],
+  '/code-and-life/': ['cname-forget', 'technical-debt'],
+  '/tools/': [
+    'add-subline-into-right-hand-menu',
+    'add-vscode-into-right-hand-menu',
+    'fanqiang',
+    'jetbrains-exclude-node_modules',
+    'light-cvm',
+    'mac-install-sublime',
+    'solo-migration',
+    'vim',
+    'vscode-vue-file-template'
   ],
-  "/back-end/": ["linux-iptables", "ubuntu-docker-jenkins"],
-  "/go/": [
-    "environment-setup-for-mac-developer",
-    "environment-setup-for-win-developer"
-  ],
-  "/node/": ["process-env"]
-};
+  '/back-end/': ['linux-iptables', 'ubuntu-docker-jenkins'],
+  '/go/': ['environment-setup-for-mac-developer', 'environment-setup-for-win-developer'],
+  '/node/': ['process-env']
+}
