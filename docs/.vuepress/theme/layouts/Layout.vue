5102c1664ff9cc5929e3608a85c30a540306d68b
mrjzhang <mrjzhang@tencent.com>
2020-07-10 00:13:17 +0800

    fix: Layout

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,19 +1,5 @@
 <template>
-  <div
-    class="theme-container"
-    :class="pageClasses"
-    @touchstart="onTouchStart"
-    @touchend="onTouchEnd"
-  >
-    <Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar" />
-
-    <div class="sidebar-mask" @click="toggleSidebar(false)"></div>
-
-    <Sidebar :items="sidebarItems" @toggle-sidebar="toggleSidebar">
-      <slot name="sidebar-top" slot="top" />
-      <slot name="sidebar-bottom" slot="bottom" />
-    </Sidebar>
-
+  <div class="home">
     <Home v-if="$page.frontmatter.home" />
 
     <Page v-else :sidebar-items="sidebarItems">
@@ -28,47 +14,23 @@
 
 <script>
 import Home from '@theme/components/Home.vue'
-import Navbar from '@theme/components/Navbar.vue'
 import Footer from '@theme/components/Footer.vue'
 import Page from '@theme/components/Page.vue'
-import Sidebar from '@theme/components/Sidebar.vue'
 import { resolveSidebarItems } from '../util'
 
 export default {
-  components: { Home, Page, Sidebar, Navbar, Footer },
-
+  components: {
+    Home,
+    Page,
+    Footer
+  },
   data() {
     return {
-      isSidebarOpen: false,
       dynamicComponent: null
     }
   },
 
   computed: {
-    shouldShowNavbar() {
-      const { themeConfig } = this.$site
-      const { frontmatter } = this.$page
-      if (frontmatter.navbar === false || themeConfig.navbar === false) {
-        return false
-      }
-      return (
-        this.$title ||
-        themeConfig.logo ||
-        themeConfig.repo ||
-        themeConfig.nav ||
-        this.$themeLocaleConfig.nav
-      )
-    },
-
-    shouldShowSidebar() {
-      const { frontmatter } = this.$page
-      return (
-        !frontmatter.home &&
-        frontmatter.sidebar !== false &&
-        this.sidebarItems.length
-      )
-    },
-
     sidebarItems() {
       return resolveSidebarItems(
         this.$page,
@@ -76,56 +38,14 @@ export default {
         this.$site,
         this.$localePath
       )
-    },
-
-    pageClasses() {
-      const userPageClass = this.$page.frontmatter.pageClass
-      return [
-        {
-          'no-navbar': !this.shouldShowNavbar,
-          'sidebar-open': this.isSidebarOpen,
-          'no-sidebar': !this.shouldShowSidebar
-        },
-        userPageClass
-      ]
     }
   },
 
   mounted() {
-    this.$router.afterEach(() => {
-      this.isSidebarOpen = false
-    })
-
     // 这样架子啊 Footer 组件，可以使 build 的时候不报不包含 window 的错误
     import('@theme/components/Footer.vue').then(Footer => {
       this.dynamicComponent = Footer.default
     })
-  },
-
-  methods: {
-    toggleSidebar(to) {
-      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
-    },
-
-    // side swipe
-    onTouchStart(e) {
-      this.touchStart = {
-        x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY
-      }
-    },
-
-    onTouchEnd(e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x
-      const dy = e.changedTouches[0].clientY - this.touchStart.y
-      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-        if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true)
-        } else {
-          this.toggleSidebar(false)
-        }
-      }
-    }
   }
 }
 </script>
