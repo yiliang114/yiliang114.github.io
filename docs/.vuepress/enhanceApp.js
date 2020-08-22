a10b8afe5760c7fed289b1cef064d8a2759441a6
mrjzhang <mrjzhang@tencent.com>
2020-08-22 22:50:39 +0800

    feat: 全局 router 守卫函数请求 vercel node cgi 的 pv api

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,7 +3,7 @@ export default ({ router }) => {
   router.beforeEach((to, from, next) => {
     // console.log('to', to);
     // console.log('from', from);
-    // new Image().src = 'https://cgi.yiliang.site/api/site';
+    new Image().src = 'https://cgi.yiliang.site/api/site';
     // new Image().src = 'http://localhost:3000/api/site';
     next();
   });
