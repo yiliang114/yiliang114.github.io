4b149ad395b227fb6ab82a2e4995af81c16fe460
mrjzhang <mrjzhang@tencent.com>
2020-10-25 22:04:26 +0800

    feat: 修改 postsDirs 使得支持多个文件夹

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -9,14 +9,16 @@ module.exports = [
         '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
     },
   ],
-  // 不蒜子统计
-  process.env.NODE_ENV === 'production'
+  ...(process.env.NODE_ENV === 'production'
     ? [
-        'script',
-        {
-          async: true,
-          src: '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js',
-        },
+        // 不蒜子统计
+        [
+          'script',
+          {
+            async: true,
+            src: '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js',
+          },
+        ],
       ]
-    : [],
+    : []),
 ];
