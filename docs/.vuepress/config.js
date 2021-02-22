7c6cc80deb881023d78d7fd45ed48b9847415c2a
mrjzhang <mrjzhang@tencent.com>
2021-02-23 00:43:25 +0800

    chore: docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -102,7 +102,7 @@ module.exports = {
       { text: '文章', link: '/posts/', exact: false },
       { text: '算法', link: '/arithmetic/', exact: false },
       // { text: '分类', link: '/posts/categories/', exact: true },
-      { text: '私语', link: '/whisper', exact: false },
+      // { text: '私语', link: '/whisper', exact: false },
       { text: '友链', link: '/links/', exact: false },
       // {
       //   text: '关于我',
